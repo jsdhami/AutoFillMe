f337fd96056ba788d150c728de95bf2104ca9908
Janak singh dhami <info@jsdhami.com.np>
2024-11-15 21:40:59 +0545

    added

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,9 +2,9 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js PWA',
-    short_name: 'NextPWA',
-    description: 'A Progressive Web App built with Next.js',
+    name: 'AutofillMe',
+    short_name: 'AutofillMe',
+    description: 'A web application that help to fill the form automatically.',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
